e23bccc3f5b4fcdb0325020a19e2ce8d6413facb
dalsun <ddy07068@gmail.com>
2023-07-14 01:19:37 +0900

    컴포넌트 구현 시작

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Header from './component/Header';
-import Footer from './component/Footer';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
